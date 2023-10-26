5ecd22c3112a50c8d2a2bcf5014966ed4ae1f044
Nur Aisyah Mohamad Ihsan <64122100+aisyahihsan@users.noreply.github.com>
2023-10-26 10:23:01 +0800

    Add files via upload

---
+++ CheeseHouseCafe/CheeseHouseCafe.cpp
@@ -0,0 +1,16 @@
+// CheeseHouseCafe.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+void main()
+
+{
+	cout << endl;
+	cout << "Welcome to Cheese House Cafe" << endl;
+
+	system("pause");
+}
